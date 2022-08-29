51a3c5665d2ac19202f99fbeb79d2e77ecd798f5
Miracle Gabriel <sixpathdev@gmail.com>
2022-08-29 13:02:40 +0100

    test cors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getRecords()
+  }, [])
+
+  const getRecords = async () => {
+    const response = await axios.get(
+      'https://expectoo-csv-test.herokuapp.com/records'
+    )
+    if (response.data.status === 'success') {
+      setData(response.data.data)
+    }
+    console.log(response.data.data)
+  }
+
+  const uploadCsv = async (e) => {
+    e.preventDefault()
+    try {
+      const formData = new FormData()
+      formData.append('csvfile', selectedFile)
+      const response = await axios.post(
+        'https://expectoo-csv-test.herokuapp.com/upload-csv',
+        formData
+      )
+      if (response.data.status === 'success') {
+        alert('Uploaded successfully')
+      }
+      console.log(response)
+    } catch (error) {
+      alert(error)
+    }
+  }
+  useEffect(() => {
+    console.log(selectedFile)
+  }, [selectedFile])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={uploadCsv} encType='multipart/formdata'>
+        <input
+          type='file'
+          onChange={(e) => setSelectedFile(e.target.files[0])}
+        />
+        <button type='submit'>Upload CSV file</button>
+      </form>
+
+      <table className='table text-center mt-5'>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Comment</th>
+            <th scope="col">Author</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.comment}</td>
+              <td>{item.author}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
